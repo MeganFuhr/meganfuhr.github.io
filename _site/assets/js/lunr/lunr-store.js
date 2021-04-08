72a4f2d9441e6e5e52caa3b706a1026acf308a09
Megan <8890846+MeganFuhr@users.noreply.github.com>
2021-04-08 16:37:51 -0400

    beeping all the boops

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Earlier Post!",
+        "excerpt":"# Welcome  HUZZAH!!  I hope you like it!","categories": [],
+        "tags": [],
+        "url": "/EarlierPostt/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"# Welcome  **Hello world**, this is my first Jekyll blog post.  I hope you like it!","categories": [],
+        "tags": [],
+        "url": "/SamplePost/",
+        "teaser": null
+      }]
